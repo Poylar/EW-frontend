ac7baea6890070d7ff46ebaf9c82cfc65b6dac9f
Poylar <doalesk8@gmail.com>
2022-10-16 06:33:51 +0800

    deploy

--- src/assets/js/StackContainer.js
+++ src/assets/js/StackContainer.js
@@ -2,19 +2,20 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 const panels = gsap.utils.toArray(".js-stack-container");
+panels.pop();
 panels.forEach((panel, i) => {
-  console.log(getComputedStyle(panel).getPropertyValue("--margin-bottom"));
   var tl = gsap
     .timeline({
       scale: 0.8,
       scrollTrigger: {
         trigger: panel,
-        scrub: 0.3,
-        start: "top -40",
-        end: "+=" + panel.offsetHeight * (panels.length - (i + 1)),
-        pin: i === panels.length ? false : true,
+        scrub: 0.4,
+        start: "bottom 70%",
+        end: "+=" + (panel.offsetHeight + 80),
+        pin: true,
         pinSpacing: false,
+        onRefresh: () => gsap.set(panel, { transformOrigin: "center " + (panel.offsetHeight - window.innerHeight / 2) + "px" }),
       },
     })
-    .to(panel, { scale: i === panels.length - 1 ? 1 : 0.7, transformOrigin: "bottom center" });
+    .to(panel, { opacity: 0, scale: 0.7, transformOrigin: "center center" });
 });
