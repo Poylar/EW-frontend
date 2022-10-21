56e5f051c053d78a5f71d18bb6081d8e33873431
Poylar <doalesk8@gmail.com>
2022-10-21 12:57:27 +0800

    deploy

--- src/assets/js/SmoothScroll.js
+++ src/assets/js/SmoothScroll.js
@@ -24,10 +24,49 @@ let smoother = ScrollSmoother.create({
   effects: true,
   onUpdate: (self) => {
     let smoothDirection = self.getVelocity() < 0 ? -1 : 1;
-    if (self.scrollTop() > 400) {
+    if (self.scrollTop() > 200) {
       smoothDirection === -1 ? showHeader.play() : showHeader.reverse();
     }
   },
 });
+
+const video = document.querySelector(".js-start-video");
+const gap = getComputedStyle(document.body).getPropertyValue("--gap");
+const tl1 = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".hero-video",
+      start: "top top",
+      end: "bottom",
+      scrub: true,
+
+      pin: true,
+      pinType: "transform",
+    },
+  })
+  .to(video, {
+    delay: 0.2,
+    padding: 0,
+    duration: 2,
+    transformOrigin: "center center",
+  })
+  .to({}, { duration: 2 });
+
+const tl2 = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".features__icon",
+      start: "top center",
+      end: "bottom",
+      scrub: true,
+      pin: false,
+
+
+      markers: true,
+    },
+  })
+  .to(".features__src", {
+    rotate: 360,
+  });
 
 export default smoother;
